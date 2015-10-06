4778e096cc9f2b421498a4c03c9b9dae5d28b88f
olatzgenua <olatz.genua@gmail.com>
2015-10-06 12:47:41 +0200

    portafolio

---
+++ htmlTrabajos/footer.php
@@ -0,0 +1,22 @@
+<!--FOOTER-->
+
+    <div id="contactar">
+        <ul>
+            <li class="copy">&copy; Copyright Olatz Genua</li>
+            <li class="email">olatz.genua@gmail.com</li>
+        </ul>
+        <ul class="redes">
+
+            <li class="linkedin">
+                <a href="http://es.linkedin.com/pub/olatz-genua/2b/877/aa0/" target="_blank">Linkedin</a>
+            </li>
+
+            <li class="twitter">
+                <a href="http://twitter.com/olatzgenu" target="_blank">twitter</a>
+            </li>
+
+
+
+        </ul><!--cierre redes-->
+    </div>
+
