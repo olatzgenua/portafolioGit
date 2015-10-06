97274fd4441545633cfea21e32e9272bca2673bf
olatzgenua <olatz.genua@gmail.com>
2015-10-06 09:39:57 +0200

    portafolio

---
+++ js/bsa2.js
@@ -0,0 +1,24 @@
+document.write('<link href="http://www.queness.com/assets/css/bsa2.css" rel="stylesheet" type="text/css" media="screen" />');
+document.write('<div id="adpack">');
+document.write('<div id="bsap_1289670" class="bsarocks bsap_3961f1e3e7b83f3fd5733c0d91cc10ed"></div>');
+document.write('</div>');
+
+
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-7781887-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+  
+	(function(){
+	  var bsa = document.createElement('script');
+	     bsa.type = 'text/javascript';
+	     bsa.async = true;
+	     bsa.src = '//s3.buysellads.com/ac/bsa.js';
+	  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
+	})();
